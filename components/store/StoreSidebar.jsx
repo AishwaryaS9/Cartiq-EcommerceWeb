603da5d73e6660bbca99462354f7d26f118b428e
AishwaryaS9 <aishwaryasudupi9@gmail.com>
2025-10-13 10:26:54 +0530

    feat: Implemented subscription plans and orders

--- components/store/StoreSidebar.jsx
+++ components/store/StoreSidebar.jsx
@@ -4,7 +4,7 @@ import { HomeIcon, LayoutListIcon, SquarePenIcon, SquarePlusIcon } from "lucide-
 import Image from "next/image"
 import Link from "next/link"
 
-const StoreSidebar = ({ storeInfo }) => {
+const StoreSidebar = ({storeInfo}) => {
 
     const pathname = usePathname()
 
